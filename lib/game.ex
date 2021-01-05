6a5edb09a7814356c2b383ede93e1085cbd00a39
Piotr Patrzyk <piotr@patrzyk.me>
2021-01-05 16:27:48 +0100

    gen indices

--- lib/game.ex
+++ lib/game.ex
@@ -1,7 +1,11 @@
 defmodule Game do
   def new(%{width: width, height: height, mines: mines}) do
+    :random.seed(:erlang.now)
+    indices = for x <- 1..width, y <- 1..height do
+      {x, y}
+    end
     # TODO implement
-    IO.puts(mines)
+    IO.puts(inspect(indices))
     [1, 2, 3]
   end
 end
