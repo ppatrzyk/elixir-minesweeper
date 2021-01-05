9ac89aece825b397b44e5b15c610dd4403b139b8
Piotr Patrzyk <piotr@patrzyk.me>
2021-01-05 17:03:17 +0100

    gen neighbor

--- lib/game.ex
+++ lib/game.ex
@@ -15,4 +15,16 @@ defmodule Game do
     mine = {x, y} in mine_list
     %{mine: mine, state: :hidden}
   end
+
+  defp generate_neighbor({x_delta, y_delta}, {x, y}) do
+    {x + x_delta, y + y_delta}
+  end
+
+  def get_neighbors({x, y}) do
+    offsets = for x_delta <- -1..1, y_delta <- -1..1 do
+      {x_delta, y_delta}
+    end
+    Enum.filter(offsets, fn(el) -> el != {0, 0} end)
+    |> Enum.map(&generate_neighbor(&1, {x, y}))
+  end
 end
