74c922cab51cb3a5cd7961ab34ee5951a5096be2
Piotr Patrzyk <piotr@patrzyk.me>
2021-01-07 15:20:07 +0100

    copy sample rectangle

--- lib/scenes/home.ex
+++ lib/scenes/home.ex
@@ -3,32 +3,36 @@ defmodule Minesweeper.Scene.Home do
   require Logger
 
   alias Scenic.Graph
-  alias Scenic.ViewPort
-
   import Scenic.Primitives
   # import Scenic.Components
 
   @note "Init example scenic"
-
   @text_size 24
+
+  @width 1024
+  @height 720
+
+  @grid [
+    rect_spec(
+      {50, 60},
+      fill: :khaki,
+      stroke: {4, :green}
+    ),
+  ]
+
+  @graph Graph.build(font: :roboto, font_size: @text_size)
+    |> add_specs_to_graph([
+      text_spec(@note, translate: {300, 200}),
+      rect_spec({@width, @height}),
+      group_spec(@grid, translate: {20, 20})
+    ])
 
   # ============================================================================
   # setup
 
   # --------------------------------------------------------
-  def init(_, opts) do
-    # get the width and height of the viewport. This is to demonstrate creating
-    # a transparent full-screen rectangle to catch user input
-    {:ok, %ViewPort.Status{size: {width, height}}} = ViewPort.info(opts[:viewport])
-
-    graph =
-      Graph.build(font: :roboto, font_size: @text_size)
-      |> add_specs_to_graph([
-        text_spec(@note, translate: {width/2, height/2}),
-        rect_spec({width, height})
-      ])
-
-    {:ok, graph, push: graph}
+  def init(_, _opts) do
+    {:ok, @graph, push: @graph}
   end
 
   def handle_input(event, _context, state) do
